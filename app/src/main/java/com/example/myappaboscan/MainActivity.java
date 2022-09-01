f586b24ed11fbad7e0c0b4a6e4694675dfe858ad
Carloslaink <Anthonycarlosr@gmail.com>
2022-09-01 17:42:42 -0400

    FirstLogin

--- app/src/main/java/com/example/myappaboscan/MainActivity.java
+++ app/src/main/java/com/example/myappaboscan/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myappaboscan;
-
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConstraintLayout Constraintlayout = findViewById(R.id.mainLayout);
+        AnimationDrawable AnimationDrawable = (AnimationDrawable) Constraintlayout.getBackground();
+        AnimationDrawable.setEnterFadeDuration(2500);
+        AnimationDrawable.setExitFadeDuration(5000);
+        AnimationDrawable.start();
+
+
     }
 }
